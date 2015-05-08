dc29a3da4a9710bfc1d5778aefa5a2e4fb9f2038
lars <lars@lars-desktop>
2015-05-08 21:44:50 +0200

    Add extra check to avoid IllegalArgumentException from InputStream.

--- src/se/troed/plugin/Courier/FramedLetterRenderer.java
+++ src/se/troed/plugin/Courier/FramedLetterRenderer.java
@@ -28,20 +28,22 @@ public class FramedLetterRenderer extends MapRenderer {
         super(false); // Framed Letters are not contextual (i.e same for all players)
         plugin = p;
 
-        if(clearImage == null) {
+        if (clearImage == null) {
             try {
                 InputStream is = plugin.getClass().getResourceAsStream("/framed.png");
-                framed = ImageIO.read(is);
-                is.close();
+                if (is != null) {
+                    framed = ImageIO.read(is);
+                    is.close();
+                }
             } catch (IOException e) {
                 plugin.getCConfig().clog(Level.WARNING, "Unable to find framed.png in .jar");
                 e.printStackTrace();
             }
-            if(framed != null) {
+            if (framed != null) {
                 plugin.getCConfig().clog(Level.FINE, "framed.png found");
                 clearImage = MapPalette.imageToBytes(framed);
             } else {
-                clearImage = new byte[128*128];
+                clearImage = new byte[128 * 128];
             }
         }
     }
@@ -51,15 +53,15 @@ public class FramedLetterRenderer extends MapRenderer {
     // https://bukkit.atlassian.net/browse/BUKKIT-476
     @Override
     public void render(MapView map, MapCanvas canvas, Player player) {
-        if(map.getCenterX() == Courier.MAGIC_NUMBER && map.getId() != plugin.getCourierdb().getCourierMapId()) {
+        if (map.getCenterX() == Courier.MAGIC_NUMBER && map.getId() != plugin.getCourierdb().getCourierMapId()) {
             // it's a Courier map in an ItemFrame. We get called when it's in a loaded chunk. Player doesn't
             // even need to be near it. Performance issues galore ...
             Letter letter = plugin.getTracker().getLetter(map.getCenterZ());
-            if(letter != null && letter.getDirty()) {
+            if (letter != null && letter.getDirty()) {
                 plugin.getCConfig().clog(Level.FINE, "Rendering a Courier ItemFrame Letter (" + letter.getId() + ") on Map (" + map.getId() + ")");
-                for(int j = 0; j < CANVAS_HEIGHT; j++) {
-                    for(int i = 0; i < CANVAS_WIDTH; i++) {
-                        canvas.setPixel(i, j, clearImage[j*128+i]);
+                for (int j = 0; j < CANVAS_HEIGHT; j++) {
+                    for (int i = 0; i < CANVAS_WIDTH; i++) {
+                        canvas.setPixel(i, j, clearImage[j * 128 + i]);
 //                        canvas.setPixel(i, j, MapPalette.TRANSPARENT);
 //                        canvas.setPixel(i, j, MapPalette.LIGHT_BROWN);
                     }
@@ -67,26 +69,26 @@ public class FramedLetterRenderer extends MapRenderer {
 
                 int drawPos = HEADER_POS;
 
-                if(letter.getHeader() != null) {
+                if (letter.getHeader() != null) {
                     canvas.drawText(0, MinecraftFont.Font.getHeight() * drawPos, MinecraftFont.Font, letter.getHeader());
-                    LetterRenderer.drawLine(canvas, 10, MinecraftFont.Font.getHeight() * (drawPos+1) +
-                            (int)(MinecraftFont.Font.getHeight() * 0.4), CANVAS_WIDTH-11, MapPalette.DARK_BROWN);
+                    LetterRenderer.drawLine(canvas, 10, MinecraftFont.Font.getHeight() * (drawPos + 1)
+                            + (int) (MinecraftFont.Font.getHeight() * 0.4), CANVAS_WIDTH - 11, MapPalette.DARK_BROWN);
                     drawPos = BODY_POS;
                 }
 
                 canvas.drawText(letter.getLeftMarkerPos(), MinecraftFont.Font.getHeight(), MinecraftFont.Font, letter.getLeftMarker());
                 canvas.drawText(letter.getRightMarkerPos(), MinecraftFont.Font.getHeight(), MinecraftFont.Font, letter.getRightMarker());
 
-                if(letter.getMessage() != null) {
+                if (letter.getMessage() != null) {
                     canvas.drawText(0,
-                                    MinecraftFont.Font.getHeight() * drawPos,
-                                    MinecraftFont.Font, Letter.MESSAGE_COLOR + letter.getMessage());
+                            MinecraftFont.Font.getHeight() * drawPos,
+                            MinecraftFont.Font, Letter.MESSAGE_COLOR + letter.getMessage());
                 }
 
-                if(letter.getDisplayDate() != null) {
+                if (letter.getDisplayDate() != null) {
                     canvas.drawText(letter.getDisplayDatePos(),
-                                    0,
-                                    MinecraftFont.Font, Letter.DATE_COLOR + letter.getDisplayDate());
+                            0,
+                            MinecraftFont.Font, Letter.DATE_COLOR + letter.getDisplayDate());
                 }
                 letter.setDirty(false);
                 player.sendMap(map);
